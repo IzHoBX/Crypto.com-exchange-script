1c40f6d22767277ffbf9b35ef7481e3f54d9d260
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-08 10:34:54 +0800

    Implements buy-if-price-changed sell-immediate algorithm

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ import time
 
 MARKET_SYM = "linkcro"
 EXECUTION_INTERVAL = 1
+ORDER_DONE_STATUS_CODE = 2
+DEFAULT_AMOUNT_TO_BUY_IN_TARGET = 1
 
 # by default only returns normal (available for use balance)
 def getBalance(sym):
@@ -20,13 +22,40 @@ def getLowestSellingPrice(sym):
 def getHighestBuyingPrice(sym):
     return apiHelper.depth(sym)['data']['tick']['bids'][0][0]
 
-apiHelper = APIHelper.CryptoAPI(sys.argv[1], sys.argv[2])
-#print(apiHelper.create_order("xrpcro", "BUY", 4.00, 1))
-#print(getBalance(apiHelper.balance(), "cro"))
-#print(getHighestBuyingPrice("linkcro"))
-
-while True:
+def getPriceToBuyAt():
     sell = getLowestSellingPrice(MARKET_SYM)
     buy = getHighestBuyingPrice(MARKET_SYM)
-    print(sell)
+    return round(min(buy+0.1, buy+sell/2), 2)
+
+def updateBuyingOids(buyingOids):
+    currentBuyingPrice = getHighestBuyingPrice(MARKET_SYM)
+    for i in range(0, len(buyingOids)):
+        info = apiHelper.get_order(MARKET_SYM, buyingOids[i])
+        if info['data']['order_info']['price'] < currentBuyingPrice:
+            apiHelper.cancel_order(MARKET_SYM, buyingOids[i])
+            apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+            buyingOids = buyingOids[:i] + buyingOids[i+1:]
+        elif info['data']['order_info']['price'] == currentBuyingPrice:
+            if apiHelper.get_ordst(MARKET_SYM, lastBuyingOrder[0]) == ORDER_DONE_STATUS_CODE:
+                apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+                buyingOids = buyingOids[:i] + buyingOids[i+1:]
+        else:#if my buying price is higher than current  highest buying price, that means order completed
+            apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+            buyingOids = buyingOids[:i] + buyingOids[i+1:]
+
+    return buyingOids
+
+apiHelper = APIHelper.CryptoAPI(sys.argv[1], sys.argv[2])
+
+buyingOids = []
+lastBuyingPrice = -1
+
+while True:
+    buyingOids = updateBuyingOids(buyingOids)
+    priceToBuyAt = getPriceToBuyAt()
+    if lastBuyingPrice == priceToBuyAt:
+        continue
+    elif float(getBalance("cro")) > priceToBuyAt * DEFAULT_AMOUNT_TO_BUY_IN_TARGET:
+        buyingOids.append(apiHelper.create_order(MARKET_SYM, "BUY", priceToBuyAt, DEFAULT_AMOUNT_TO_BUY_IN_TARGET)['data']['order_id'])
+        lastBuyingPrice = priceToBuyAt
     time.sleep(EXECUTION_INTERVAL)
