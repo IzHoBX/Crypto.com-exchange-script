3593e97c8c08407c80c3072e0864fa2d5c7844e3
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-08 10:53:47 +0800

    Adds verbose message printing

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import APIHelper
 import sys
 import time
+import datetime
 
 MARKET_SYM = "linkcro"
 EXECUTION_INTERVAL = 1
@@ -28,22 +29,35 @@ def getPriceToBuyAt():
     return round(min(buy+0.1, buy+sell/2), 2)
 
 def updateBuyingOids(buyingOids):
+    print("updating buying orders at time: " + str(datetime.datetime.now()))
     currentBuyingPrice = getHighestBuyingPrice(MARKET_SYM)
     for i in range(0, len(buyingOids)):
         info = apiHelper.get_order(MARKET_SYM, buyingOids[i])
         if info['data']['order_info']['price'] < currentBuyingPrice:
+            print("market buying price has rose above our bid at: " + currentBuyingPrice + " cancelling order at: " + info['data']['order_info']['price'])
             apiHelper.cancel_order(MARKET_SYM, buyingOids[i])
-            apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+            if info['data']['order_info']['deal_volume'] > 0:
+                apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+                print("created order selling order with price: " + round(info['data']['order_info']['price']*1.003, 2))
+            else:
+                print("Nothing was bought, no need to sell")
             buyingOids = buyingOids[:i] + buyingOids[i+1:]
         elif info['data']['order_info']['price'] == currentBuyingPrice:
+            print("market price equal to our bid at: " + info['data']['order_info']['price'])
             if apiHelper.get_ordst(MARKET_SYM, lastBuyingOrder[0]) == ORDER_DONE_STATUS_CODE:
                 apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
+                print("Our order complated. Created order selling order with price: " + round(info['data']['order_info']['price']*1.003, 2))
                 buyingOids = buyingOids[:i] + buyingOids[i+1:]
         else:#if my buying price is higher than current  highest buying price, that means order completed
+            print("market buying price has fallen below our bid at: " + currentBuyingPrice + " creating sell order at: " + round(info['data']['order_info']['price']*1.003, 2))
             apiHelper.create_order(MARKET_SYM, "SELL", round(info['data']['order_info']['price']*1.003, 2), info['data']['order_info']['deal_volume'])
             buyingOids = buyingOids[:i] + buyingOids[i+1:]
 
     return buyingOids
+
+def printCurrentBuyingOrders(buyingOids):
+    for id in buyingOids:
+        print(id + " " + apiHelper.get_order(MARKET_SYM, id)['data']['order_info']['price'])
 
 apiHelper = APIHelper.CryptoAPI(sys.argv[1], sys.argv[2])
 
